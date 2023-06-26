bf008d3ae30c9bbb90646d1606b3024f17d6569e
hemen eugene <hemeneugene@yahoo.com>
2023-06-26 14:25:24 +0100

    Add project files.

---
+++ PracticeGit/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PracticeGit.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
